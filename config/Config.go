20826603f7c3ccf562e5fab2b7f40e1509fb1dc4
Grand <Grand@fanruan.com>
2022-11-17 00:06:09 +0800

    update: 删除阿里云&&华为云 修改下七牛的上传方式

--- config/Config.go
+++ config/Config.go
@@ -5,18 +5,6 @@ import (
 )
 
 type Config struct {
-	Type         string `ini:"storage.type"`
-	Endpoint     string `ini:"alioss.endpoint"`
-	AccessKey    string `ini:"alioss.accessKey"`
-	AccessSecret string `ini:"alioss.accessSecret"`
-	BucketName   string `ini:"alioss.bucketName"`
-	Folder       string `ini:"alioss.folder"`
-	//华为OBS
-	HwEndpoint   string `ini:"huaweiobs.endpoint"`
-	HwAk         string `ini:"huaweiobs.ak"`
-	HwSk         string `ini:"huaweiobs.sk"`
-	HwBucketName string `ini:"huaweiobs.bucketName"`
-	HwFolder     string `ini:"huaweiobs.folder"`
 	//七牛云
 	QiniuApiHost   string `ini:"qiniu.apiHost"`
 	QiniuAccessKey string `ini:"qiniu.accessKey"`
@@ -24,12 +12,6 @@ type Config struct {
 	QiniuBucket    string `ini:"qiniu.bucket"`
 	QiniuFolder    string `ini:"qiniu.folder"`
 	QiniuUseHTTPS  string `ini:"qiniu.usehttps"`
-
-	//通知相关
-	NotifyDingToken string `ini:"notify.dingtalkToken"`
-	NotifyWxToken   string `ini:"notify.wxKey"`
-	PodName         string `ini:"podName"`
-	DumpFile        string `ini:"dump.filepath"`
 }
 
 var inifile = "jvmdump4k8s.ini"
--- jvmdump4k8s.go
+++ jvmdump4k8s.go
@@ -3,36 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
-	"jvmdump4k8s/config"
 	"jvmdump4k8s/qiniu"
 	"jvmdump4k8s/util"
 )
 
 var (
-	enabledd bool   //是否启用通知
-	enablewx bool   //是否启用微信通知
 	dumpFile string //dump的文件
-	pod      string //podname
-
 )
-
-func init() {
-	//fmt.Println("init....")
-
-}
 
 func main() {
 	//parseCli()
 	//fmt.Println("start invoke dump...")
 	//flag.Parse()
 	dumpFile := "./zhw.dump"
-
-	if "" != pod {
-		config.GlobalConfig.PodName = "1"
-	}
-	if "" != dumpFile {
-		config.GlobalConfig.DumpFile = "./zhw.dump"
-	}
 	fmt.Printf("dumpFile %s \n", dumpFile)
 	//dump文件是否存在
 	exist, err := util.FileExists(dumpFile)
@@ -50,24 +33,10 @@ func main() {
 
 //解析命令行参数
 func parseCli() {
-
 	flag.StringVar(&dumpFile, "f", "", "-f xx.dump")
-	flag.StringVar(&pod, "pod", "", "pod")
-	if "" != config.GlobalConfig.NotifyDingToken {
-		enabledd = true
-	}
-	if "" != config.GlobalConfig.NotifyWxToken {
-		enablewx = true
-	}
 }
 
 //Storage
 func uploadStorage(file string) string {
-	type_ := config.GlobalConfig.Type
-	switch type_ {
-	case "qiniu":
-		return qiniu.Upload(file)
-	default:
-		panic(fmt.Sprintf("不支持文件存储类型%s", type_))
-	}
+	return qiniu.Upload(file)
 }
